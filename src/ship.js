2a9b4c139279b0bd076e913d53911e9ac022dcba
Nico <nicodiazan@gmail.com>
2023-09-18 14:55:06 +0200

    Fix some tests and continue development

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,18 @@
 export default class Ship {
+	constructor(length) {
+		this.length = length;
+	}
 
-    constructor(length) {
-        
-    }
+	set length(len) {
+		const newLen = parseInt(len);
+		if (newLen !== Number(len)) {
+			throw new Error(
+				"Ship length of wrong type or in wrong format (" + len + ")"
+			);
+		}
+		this._length = newLen;
+	}
+	get length() {
+		return this._length;
+	}
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -10,12 +10,10 @@ test("Test Ship length 2", () => {
 	expect(ship.length).toBe(2);
 });
 test("Test Ship length (wrong input 1)", () => {
-	const ship = new Ship(-1);
-	expect(ship.length).toThrowError(/length/);
+	expect(() => new Ship(-1)).toThrowError(/length/);
 });
 test("Test Ship length (wrong input 1)", () => {
-	const ship = new Ship("a");
-	expect(ship.length).toThrowError(/type/);
+	expect(() => new Ship("a")).toThrowError(/type/);
 });
 test("Test Ship length (no input)", () => {
 	const ship = new Ship();
