0a8844a95ece6f55448f25358c4ce9fd7604c50c
Nico <nicodiazan@gmail.com>
2023-09-18 14:37:13 +0200

    Add some tests for the Ship class

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,3 @@
 export default class Ship {
-	test(a) {
-		return a;
-	}
+
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,7 +1,19 @@
 import { expect, test } from "vitest";
 import Ship from "../src/ship";
 
-test("sample test", () => {
-	const ship = new Ship();
-	expect(ship.test("a")).toBe("a");
+test("Test Ship length 1", () => {
+    const ship = new Ship(4);
+	expect(ship.length).toBe(4);
+});
+test("Test Ship length 2", () => {
+    const ship = new Ship(2);
+	expect(ship.length).toBe(2);
+});
+test("Test Ship length (wrong input 1)", () => {
+    const ship = new Ship(-1);
+	expect(ship.length).toThrowError(/length/);
+});
+test("Test Ship length (wrong input 1)", () => {
+    const ship = new Ship("a");
+	expect(ship.length).toThrowError(/type/);
 });
