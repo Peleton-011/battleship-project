b1f4764818bce3969ab6bffd3e49662b1b51f711
Nico <nicodiazan@gmail.com>
2023-09-18 14:38:54 +0200

    Finish up Ship length tests

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,18 +2,22 @@ import { expect, test } from "vitest";
 import Ship from "../src/ship";
 
 test("Test Ship length 1", () => {
-    const ship = new Ship(4);
+	const ship = new Ship(4);
 	expect(ship.length).toBe(4);
 });
 test("Test Ship length 2", () => {
-    const ship = new Ship(2);
+	const ship = new Ship(2);
 	expect(ship.length).toBe(2);
 });
 test("Test Ship length (wrong input 1)", () => {
-    const ship = new Ship(-1);
+	const ship = new Ship(-1);
 	expect(ship.length).toThrowError(/length/);
 });
 test("Test Ship length (wrong input 1)", () => {
-    const ship = new Ship("a");
+	const ship = new Ship("a");
 	expect(ship.length).toThrowError(/type/);
+});
+test("Test Ship length (no input)", () => {
+	const ship = new Ship();
+	expect(ship.length).toBeTypeOf("number");
 });
