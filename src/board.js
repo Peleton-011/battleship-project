776a40582423a89f7992ecefc5d467cf5a930cf7
Nico <nicodiazan@gmail.com>
2023-09-19 18:19:05 +0200

    Add some basic properties and such for the Gameboard class

--- src/board.js
+++ src/board.js
@@ -1,3 +1,26 @@
 export default class GameBoard {
-    
+	constructor(size, boatList) {
+		this.board = this._makeBoard(size || 10);
+		this.ships = boatList || [2, 3, 3, 4, 5];
+	}
+
+	_makeBoard(size) {
+		size = this._testPositiveInt(size);
+		return new Array(size).fill(new Array(size).fill(null));
+	}
+
+	_testPositiveInt(input) {
+		const num = parseInt(input);
+		if (num !== Number(num)) {
+			throw new Error(
+				"Wrong type or format (" + num + "), expected a positive int"
+			);
+		}
+		if (num < 1) {
+			throw new Error(
+				"Wrong type or format (" + num + "), expected a positive int"
+			);
+		}
+		return num;
+	}
 }
