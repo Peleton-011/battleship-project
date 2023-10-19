d6d8ecd82156ee0f5a57a8f1a124d7600e4fe392
Nico <nicodiazan@gmail.com>
2023-10-19 19:08:04 +0200

    Minor changes

--- src/board.js
+++ src/board.js
@@ -10,8 +10,29 @@ export default class GameBoard {
 	}
 
 	placeShip(index, [x, y], rotation) {
+		//Check if the ship has been placed
+		if (typeof this.ships[index] !== "number")
+			return new Error("Ship " + index + " already placed");
+
 		const len = this._testPositiveInt(this.ships[index]);
-        const coords = Ship.getShipCoords(len, [x, y], rotation);
+		const coords = Ship.getShipCoords(len, [x, y], rotation);
+
+		//TO-DO: Refactor these loops vvvv (?)
+
+		//Check if the coords are available
+		for (let i = 0; i < coords.length; i++) {
+			const currentCoords = coords[i];
+			if (this.board[currentCoords[0]][currentCoords[1]] !== null)
+				return new Error(
+					"Tile " +
+						currentCoords[0] +
+						", " +
+						currentCoords[1] +
+						" already occupied (" +
+						this.board[currentCoords[0]][currentCoords[1]] +
+						")"
+				);
+		}
 
 		for (let i = 0; i < coords.length; i++) {
 			const currentCoords = coords[i];
--- tests/board.test.js
+++ tests/board.test.js
@@ -12,28 +12,26 @@ test("Boat List", () => {
 	expect(testBoard.ships).toHaveLength(5);
 });
 test("Place Boat 1", () => {
-    testBoard.placeShip(1, [1,1], 1)
+	testBoard.placeShip(1, [1, 1], 1);
 	expect(testBoard.board[1][1]).toBe(1);
 	expect(testBoard.board[2][1]).toBe(1);
 	expect(testBoard.board[3][1]).toBe(1);
 	expect(testBoard.board[4][1]).toBe(null);
 });
 
-test("Place Boat 1", () => {
-    testBoard.placeShip(2, [1,1], 1)
+test("Place Boat 2", () => {
+	testBoard.placeShip(3, [1, 1], 1);
+	expect(testBoard.board[2][2]).toBe(3);
+	expect(testBoard.board[3][2]).toBe(3);
+	expect(testBoard.board[4][2]).toBe(3);
+	expect(testBoard.board[5][2]).toBe(3);
+	expect(testBoard.board[6][2]).toBe(null);
+});
+
+test("Place Boat 3", () => {
+	testBoard.placeShip(2, [1, 1], 1);
 	expect(testBoard.board[1][1]).toBe(2);
 	expect(testBoard.board[2][1]).toBe(2);
 	expect(testBoard.board[3][1]).toBe(2);
 	expect(testBoard.board[4][1]).toBe(null);
 });
-
-test("Place Boat 3", () => {
-    testBoard.placeShip(3, [2,2], 1)
-	expect(testBoard.board[2][2]).toBe(3);
-	expect(testBoard.board[3][2]).toBe(3);
-	expect(testBoard.board[4][2]).toBe(3);
-	expect(testBoard.board[5][2]).toBe(3);
-    expect(testBoard.board[6][2]).toBe(null);
-});
-
-
