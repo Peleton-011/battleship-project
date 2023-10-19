166e3669ac210b573e38096c570dd151cf797002
Nico <nicodiazan@gmail.com>
2023-10-19 19:16:47 +0200

    Add some tests and update old ones

--- tests/board.test.js
+++ tests/board.test.js
@@ -16,11 +16,11 @@ test("Place Boat 1", () => {
 	expect(testBoard.board[1][1]).toBe(1);
 	expect(testBoard.board[2][1]).toBe(1);
 	expect(testBoard.board[3][1]).toBe(1);
-	expect(testBoard.board[4][1]).toBe(null);
+	expect(testBoard.board[6][1]).toBe(null);
 });
 
 test("Place Boat 2", () => {
-	testBoard.placeShip(3, [1, 1], 1);
+	testBoard.placeShip(3, [2, 2], 1);
 	expect(testBoard.board[2][2]).toBe(3);
 	expect(testBoard.board[3][2]).toBe(3);
 	expect(testBoard.board[4][2]).toBe(3);
@@ -35,3 +35,22 @@ test("Place Boat 3", () => {
 	expect(testBoard.board[3][1]).toBe(2);
 	expect(testBoard.board[4][1]).toBe(null);
 });
+
+test("Attack 1", () => {
+    testBoard.attack([1,1])
+    expect(testBoard.ships[2].hits).toBe(1)
+})
+
+test("Attack 2", () => {
+    testBoard.attack([3,3])
+    expect(testBoard.board[3][3]).toBe(-1)
+})
+
+test("Attack 3", () => {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            testBoard.attack([i,j])
+        }
+    }
+    expect(testBoard.isAllSunk()).toBe(true)
+})
