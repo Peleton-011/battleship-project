7e0639126a69ebc5005bb660302cdc45b1481c23
Nico <nicodiazan@gmail.com>
2023-11-04 14:29:24 +0100

    Add some stuff to check the UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,13 @@ function App() {
     const board1 = new GameBoard()
     const board2 = new GameBoard()
 
-    board1.placeShip(2, [1, 5], 0)
+    board1.placeShip(2, [1, 3], 1);
+    board1.attack([1,1])
+    board1.attack([3,3])
+
+    console.log(board1.ships)
+
+    console.log(board1.isAllSunk() + " finale")
 
 	return (
     <main>
