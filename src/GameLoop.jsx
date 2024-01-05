7221c0c61122b03072d5a2a23ddae46c98ef4101
Nico Diaz <nicodiazan@gmail.com>
2024-01-05 22:00:04 +0100

    Begin moving most logic out of the Player component

--- src/GameLoop.jsx
+++ src/GameLoop.jsx
@@ -12,9 +12,14 @@ const GameLoop = ({ isSinglePlayer, argSize, argShips }) => {
 	const [turnEnded, setTurnEnded] = useState(false);
 
 	const [size, setSize] = useState(argSize || 10);
-	const [ships, setShips] = useState(
+	const [ships1, setShips1] = useState(
 		(argShips || [2, 3, 3, 4, 5]).map((ship) => new Ship(ship))
 	);
+	const [ships2, setShips2] = useState(
+		(argShips || [2, 3, 3, 4, 5]).map((ship) => new Ship(ship))
+	);
+
+	const [shipsPlaced, setShipsPlaced] = useState([false, false]);
 
 	const [board1, setBoard1] = useState(_makeBoard(size));
 	const [board2, setBoard2] = useState(_makeBoard(size));
@@ -52,35 +57,176 @@ const GameLoop = ({ isSinglePlayer, argSize, argShips }) => {
 		return num;
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////
+	const getPlaceShip = (
+		board,
+		setBoard,
+		ships,
+		setShips,
+		shipsPlacedIndex
+	) => {
+		return (e, [x, y]) => {
+			if (shipsPlaced[shipsPlacedIndex]) return;
+			const index = getNextIndex(ships);
+			console.log("Clickity Click! (" + x + ", " + y + ")");
+
+			const rot = getRotation(getMousePos(e));
+
+			// console.log("Mouse position: " + mousePos.relX + "," + mousePos.relY);
+
+			const len = _testPositiveInt(ships[index].length);
+			const coords = Ship.getShipCoords(len, [x - 1, y - 1], rot);
+
+			coords.forEach(([currx, curry]) => {
+				//Check if the coords are available
+				if (board[currx][curry] !== null)
+					throw new Error(
+						"Tile " +
+							currx +
+							", " +
+							curry +
+							" already occupied (" +
+							board[currx][curry] +
+							")"
+					);
+				// console.log("placing " + x + ", " + y);
+				setBoard(updateBoard(index, [currx, curry], board));
+			});
+			console.log("about to new ship this bith");
+			setShips(
+				ships.map((ship, i) => {
+					if (i === index) {
+						return new Ship(len, x, y, rotation);
+					}
+					return ship;
+				})
+			);
+
+			if (index === ships.length - 1) {
+				console.log("ending turn");
+				setShipsPlaced({ ...shipsPlaced, [shipsPlacedIndex]: true });
+				endTurn();
+			}
+		};
+	};
+
+	function getAttack(board, setBoard, ships) {
+		return (e, coords) => {
+			const [x, y] = coords.map((coord) => coord - 1);
+			console.log("attacking " + x + ", " + y + ": " + board[x][y]);
+			if (board[x][y] < 0) {
+				return;
+			}
+			if (typeof board[x][y] !== "number") {
+				setBoard(updateBoard(-1, [x, y], board));
+				return;
+			}
+			ships[board[x][y]].hit();
+			setBoard(updateBoard(-2, [x, y], board));
+			if (isAllSunk(ships)) {
+				console.log("you win");
+			}
+			endTurn();
+		};
+	}
+
+	const onAttack = (e, x, y) => {
+		if (!enemyBoard[x][y]) {
+			const res = attackEnemy(e, x, y);
+
+			setEnemyBoard(updateBoard(res, [x, y], enemyBoard));
+		}
+	};
+	///////////////////////////////////////////////////////////////////////////
+
+	const getNextIndex = (board, ships) => {
+		const i = ships.indexOf(
+			ships.filter(
+				(ship) =>
+					!board.some((col) => col.includes(ships.indexOf(ship))) &&
+					!ship.isSunk()
+			)[0]
+		);
+		console.log(i);
+		return i;
+	};
+
+	function getRotation(mousePosition) {
+		//Select side based on mouse position (Up, down, left, right)
+		if (
+			mousePosition.relX > mousePosition.relY &&
+			mousePosition.relX < 1 - mousePosition.relY
+		) {
+			return 0;
+		} else if (
+			mousePosition.relX < mousePosition.relY &&
+			mousePosition.relX < 1 - mousePosition.relY
+		) {
+			return 3;
+		} else if (
+			mousePosition.relX > mousePosition.relY &&
+			mousePosition.relX > 1 - mousePosition.relY
+		) {
+			return 1;
+		} else if (
+			mousePosition.relX < mousePosition.relY &&
+			mousePosition.relX > 1 - mousePosition.relY
+		) {
+			return 2;
+		}
+	}
+
+	function getMousePos(e) {
+		const rect = e.target.getBoundingClientRect();
+		return {
+			x: e.clientX - rect.left,
+			y: e.clientY - rect.top,
+			relX: (e.clientX - rect.left) / rect.width,
+			relY: (e.clientY - rect.top) / rect.height,
+		};
+	}
+
+	function isAllSunk(ships) {
+		return ships
+			.filter((ship) => typeof ship != "number")
+			.every((ship) => ship.isSunk());
+	}
+
+	function updateBoard(newVal, [x, y], board) {
+		const newBoard = [...board];
+		newBoard[x][y] = newVal;
+		return newBoard;
+	}
+
 	return (
 		<div>
 			<Player
-				ships={ships}
-                setShips={setShips}
+				nextIndex={getNextIndex(board1, ships1)}
 				size={size}
 				//
 				board={board1}
-				setBoard={setBoard1}
 				enemyBoard={board2}
-				setEnemyBoard={setBoard2}
 				//
 				active={currentBoard && !turnEnded}
-				endTurn={endTurn}
 				player={p1Name}
+				//
+				placeShip={getPlaceShip(board1, setBoard1, ships1, setShips1)}
+				attack={getAttack(board2, setBoard2, ships2)}
+				shipsPlaced={shipsPlaced}
 			/>
 			<Player
-				ships={ships}
-                setShips={setShips}
+				nextIndex={getNextIndex(board2, ships2)}
 				size={size}
 				//
 				board={board2}
-				setBoard={setBoard2}
 				enemyBoard={board1}
-				setEnemyBoard={setBoard1}
 				//
 				active={!currentBoard && !turnEnded}
-				endTurn={endTurn}
 				player={p2Name}
+				//
+				placeShip={getPlaceShip(board2, setBoard2, ships2, setShips2)}
+				attack={getAttack(board1, setBoard1, ships1)}
+				shipsPlaced={shipsPlaced}
 			/>
 			{!isSinglePlayer && (
 				<NextTurn
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,188 +1,31 @@
 import React from "react";
 import Board from "./Board";
 import Ship from "../Ship";
-import { useState, useEffect, useImperativeHandle } from "react";
+import { useState } from "react";
 
 const Player = ({
 	board,
-	setBoard,
 	enemyBoard,
-	setEnemyBoard,
 	size,
-	ships,
-	setShips,
+	nextIndex,
 	active,
-	endTurn,
 	player,
-	attackEnemy,
-	ref,
+
+	placeShip,
+	attack,
+    shipsPlaced
 }) => {
-	const [shipsPlaced, setShipsPlaced] = useState(false);
 
-	const onAttack = (e, x, y) => {
-		if (!enemyBoard[x][y]) {
-			const res = attackEnemy(e, x, y);
 
-			setEnemyBoard(updateBoard(res, [x, y]));
-		}
-	};
-
-	function updateBoard(newVal, [x, y]) {
-		const newBoard = [...board];
-		newBoard[x][y] = newVal;
-		return newBoard;
-	}
-
-	const getNextIndex = () => {
-		const i = ships.indexOf(
-			ships.filter(
-				(ship) =>
-					!board.some((col) => col.includes(ships.indexOf(ship))) &&
-					!ship.isSunk()
-			)[0]
-		);
-		console.log(i);
-		return i;
-	};
-	const sendPlaceShip = (e, x, y, i) => {
-		console.log("Clickity Click! (" + x + ", " + y + ")");
-
-		console.log(ships);
-		const rect = e.target.getBoundingClientRect();
-		const mousePos = {
-			x: e.clientX - rect.left,
-			y: e.clientY - rect.top,
-			relX: (e.clientX - rect.left) / rect.width,
-			relY: (e.clientY - rect.top) / rect.height,
-		};
-
-		let rot = 0;
-		//Select side based on mouse position (Up, down, left, right)
-		if (
-			mousePos.relX > mousePos.relY &&
-			mousePos.relX < 1 - mousePos.relY
-		) {
-			rot = 0;
-		} else if (
-			mousePos.relX < mousePos.relY &&
-			mousePos.relX < 1 - mousePos.relY
-		) {
-			rot = 3;
-		} else if (
-			mousePos.relX > mousePos.relY &&
-			mousePos.relX > 1 - mousePos.relY
-		) {
-			rot = 1;
-		} else if (
-			mousePos.relX < mousePos.relY &&
-			mousePos.relX > 1 - mousePos.relY
-		) {
-			rot = 2;
-		}
-
-		// console.log("Mouse position: " + mousePos.relX + "," + mousePos.relY);
-
-		placeShip(i, [x - 1, y - 1], rot);
-		if (i === ships.length - 1) {
-			console.log("ending turn");
-			setShipsPlaced(true);
-			endTurn();
-		}
-	};
-	const sendAttack = (e, x, y) => {
-		attack([x - 1, y - 1]);
-		endTurn();
-	};
-
-	function placeShip(index, [x, y], rotation) {
-		const len = _testPositiveInt(ships[index].length);
-		const coords = Ship.getShipCoords(len, [x, y], rotation);
-
-		coords.forEach(([x, y]) => {
-			//Check if the coords are available
-			if (board[x][y] !== null)
-				throw new Error(
-					"Tile " +
-						x +
-						", " +
-						y +
-						" already occupied (" +
-						board[x][y] +
-						")"
-				);
-			// console.log("placing " + x + ", " + y);
-			setBoard(updateBoard(index, [x, y]));
-		});
-		console.log("about to new ship this bith");
-		setShips(
-			ships.map((ship, i) => {
-				if (i === index) {
-					return new Ship(len, x, y, rotation);
-				}
-				return ship;
-			})
-		);
-	}
-	function attack([x, y]) {
-		console.log("attacking " + x + ", " + y + ": " + board[x][y]);
-		if (board[x][y] < 0) {
-			return;
-		}
-		if (typeof board[x][y] !== "number") {
-			setBoard(updateBoard(-1, [x, y]));
-			return;
-		}
-		ships[board[x][y]].hit();
-		setBoard(updateBoard(-2, [x, y]));
-		if (isAllSunk()) {
-			console.log("you win");
-		}
-	}
-
-	const onClick = (e, x, y) => {
-		if (shipsPlaced === false) {
-			const i = getNextIndex();
-			if (i === -1) {
-				return sendAttack(e, x, y);
-			}
-			return ((e, x, y) => sendPlaceShip(e, x, y, i))(e, x, y);
-		}
-		return sendAttack(e, x, y);
-	};
-
-	function isAllSunk() {
-		return ships
-			.filter((ship) => typeof ship != "number")
-			.every((ship) => ship.isSunk());
-	}
-
-	function _testPositiveInt(input) {
-		const num = parseInt(input);
-		if (num !== Number(num)) {
-			throw new Error(
-				"Wrong type or format (" + num + "), expected a positive int"
-			);
-		}
-		if (num < 1) {
-			throw new Error(
-				"Wrong type or format (" + num + "), expected a positive int"
-			);
-		}
-		return num;
-	}
-
-	useImperativeHandle(ref, () => ({
-		sendAttack: sendAttack,
-	}));
 
 	return (
 		<div className={"board-wrapper" + (active ? " active" : "")}>
 			<h2>
-				{player}, {getNextIndex() >= 0 ? "place your ships" : "attack"}
+				{player}, {nextIndex >= 0 ? "place your ships" : "attack"}
 			</h2>
 			<Board
 				board={board}
-				onClick={onClick}
+				onClick={placeShip}
 				size={size}
 				isSmall={shipsPlaced}
 			/>
@@ -190,7 +33,7 @@ const Player = ({
 				<Board
 					isSmall={!shipsPlaced}
 					board={enemyBoard}
-					onClick={onAttack}
+					onClick={attack}
 					size={size}
 				/>
 			)}
